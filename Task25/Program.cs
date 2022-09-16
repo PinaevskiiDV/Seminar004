c3cd7bea3bab592f4cd7faee96ce1e974ddcfc67
Dmitrii <pinochet37@mail.ru>
2022-09-16 15:37:54 +0300

    added Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+int i;
+for(i = 0; i < b; i += 1)
+{
+    result = result * a;
+}
+Console.WriteLine($"Число {a} в степени {b} равно {result}");
