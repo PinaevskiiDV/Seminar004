217720fee784cf9e96b7a2aefa05357dc0ca75a8
Dmitrii <pinochet37@mail.ru>
2022-09-16 16:03:46 +0300

    added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while(number > 0)
+{
+    sum = sum + number % 10;
+    number /= 10;
+}
+Console.WriteLine($"Сумма цифр равна {sum}");
