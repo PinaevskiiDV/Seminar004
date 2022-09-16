6e431d45b04e7103fc6fa75cb7a5728ff7be63f6
Dmitrii <pinochet37@mail.ru>
2022-09-16 17:02:21 +0300

    added Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,12 @@
+﻿void FillArray(int[] arr)
+{
+    int lenght = arr.Length;
+    int i;
+    for(i = 0; i < lenght; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+}
+int[] array = new int[8];
+FillArray(array);
+Console.WriteLine(string.Join(",", array));
